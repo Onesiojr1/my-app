d0e6da960c18fe78fa14fa25c74485f590c55744
onesio <onesiojr1@gmail.com>
2021-04-11 16:41:33 -0300

    melhorando o estilo da lista e adicionando a descrição

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import './App.css';
 
 function App() {
 
-  
+
   const [carrinho, setCarrinho] = useState([]);
-  
+
   const [produtos, setProdutos] = useState([
 
   ]);
 
   useEffect(() => {
     fetch("http://localhost:9000/produtos")
-        .then(res => res.json())
-        .then(res => setProdutos([...res]));
+      .then(res => res.json())
+      .then(res => setProdutos([...res]));
   }, [])
-  
+
   const ordenarProdutos = (produtos) => {
-    setProdutos([...produtos.sort((a,b) => a.preco - b.preco )])
+    setProdutos([...produtos.sort((a, b) => a.preco - b.preco)])
   }
 
   const adicinarCarrinho = (produtos) => {
@@ -29,8 +29,8 @@ function App() {
     <header className="Cabecalho">
       <img src="..\img\logo2.jpg" alt="Logo"></img>
       <div className="pesquisa">
-      <input type="text" value="O que procura?" />
-      <button> Pesquisar</button>
+        <input type="text" value="O que procura?" />
+        <button> Pesquisar</button>
       </div>
       <button> Carrinho ({carrinho.length})</button>
     </header>
@@ -38,11 +38,12 @@ function App() {
     <div className="Destaque">
       <h1>Destaques</h1>
       <div className="Produtos">
-        {produtos.slice(0,3).map(produto => (
+        {produtos.slice(0, 3).map(produto => (
           <div>
             <h3>{produto.nome}</h3>
-            <img src={"http://localhost:9000"+produto.img} alt="Smash"></img>
-            <h4>{produto.preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</h4>
+            <img src={"http://localhost:9000" + produto.img} alt={produto.nome}></img>
+            <p>{produto.descricao}</p>
+            <h4>{produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h4>
             <div onClick={() => adicinarCarrinho(produtos)} >adicionar ao carrinho</div>
           </div>
         ))}
@@ -51,14 +52,16 @@ function App() {
 
     <div className="lista">
       <div className="ordenar" onClick={() => ordenarProdutos(produtos)}>Ordenar Preços</div>
-    {produtos.slice(3, produtos.length).map(produto => (
+      <div className="produtos">
+        {produtos.slice(3, produtos.length).map(produto => (
           <div>
             <h3>{produto.nome}</h3>
-            <img src={"http://localhost:9000"+produto.img} alt="Smash"></img>
-            <h4>{produto.preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</h4>
+            <img src={"http://localhost:9000" + produto.img} alt={produto.nome}></img>
+            <h4>{produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h4>
             <div onClick={() => adicinarCarrinho(produtos)} >adicionar ao carrinho</div>
           </div>
         ))}
+      </div>
     </div>
   </div>
 }
