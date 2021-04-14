ad718df0d364d1ddf849b7e2de55e6b17aed83cc
onesio <onesiojr1@gmail.com>
2021-04-13 21:18:23 -0300

    criando carrinho, busca e selecionar crescente e decrescente

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import './App.css';
 
 function App() {
 
-
   const [carrinho, setCarrinho] = useState([]);
+  const [produtos, setProdutos] = useState([]);
+  const [ordenacao, setOrdenacao] = useState('crescente');
+  const [termoBusca, setTermoBusca] = useState("");
 
-  const [produtos, setProdutos] = useState([
-
-  ]);
 
   useEffect(() => {
     fetch("http://localhost:9000/produtos")
@@ -17,48 +16,97 @@ function App() {
       .then(res => setProdutos([...res]));
   }, [])
 
-  const ordenarProdutos = (produtos) => {
-    setProdutos([...produtos.sort((a, b) => a.preco - b.preco)])
+  const handleSelect = (e) => {
+    const order = e.target.value;
+    setOrdenacao(order);
+
+    if (order === 'crescente') {
+      setProdutos([...produtos.sort((a, b) => a.preco - b.preco)]);
+    }
+
+    if (order === 'decrescente') {
+      setProdutos([...produtos.sort((a, b) =>b.preco - a.preco )]);
+    }
   }
 
-  const adicinarCarrinho = (produtos) => {
-    setCarrinho([...carrinho, produtos]);
+  const adicinarCarrinho = (produto) => {
+    setCarrinho([...carrinho, { ...produto }]);
+  }
+
+  const removerCarrinho = (produto) => {
+    setCarrinho(carrinho.filter(produtoCarrinho => produtoCarrinho !== produto));
+  }
+
+  const procuraProduto = (e) => {
+    setTermoBusca(e.target.value);
+  } 
+
+  const itemProduto = (produto) => {
+    return (
+      <div>
+        <h3>{produto.nome}</h3>
+        <img src={"http://localhost:9000" + produto.img} alt={produto.nome}></img>
+        <h4>{produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h4>
+        <div onClick={() => adicinarCarrinho(produto)} >adicionar ao carrinho</div>
+      </div>
+    )
   }
 
   return <div className="App">
     <header className="Cabecalho">
       <img src="..\img\logo2.jpg" alt="Logo"></img>
       <div className="pesquisa">
-        <input type="text" value="O que procura?" />
+        <input type="text" onChange={(e) => procuraProduto(e)} />
         <button> Pesquisar</button>
       </div>
       <button> Carrinho ({carrinho.length})</button>
     </header>
 
-    <div className="Destaque">
-      <h1>Destaques</h1>
+    { termoBusca === "" && (
+      <div className="Destaque">
+        <h1>Destaques</h1>
+        <div className="Produtos">
+          {produtos.slice(0, 3).map(produto => (
+            <div>
+              <h3>{produto.nome}</h3>
+              <img src={"http://localhost:9000" + produto.img} alt={produto.nome}></img>
+              <p>{produto.descricao}</p>
+              <h4>{produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h4>
+              <div onClick={() => adicinarCarrinho(produto)} >adicionar ao carrinho</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    )}
+    
+    <div className="lista">
+      <select value={ordenacao} onChange={(e) => handleSelect(e)}>
+        <option value={'crescente'}>Preço crescente</option>
+        <option value={'decrescente'}>Preço decrescente</option>
+      </select>
+      <div className="produtos"> 
+        { termoBusca === "" 
+          ? produtos.slice(3, produtos.length).map(itemProduto)
+          : produtos.filter((produto) => {
+              const busca = termoBusca.toLowerCase()
+              const nome = produto.nome.toLowerCase()
+              const categoria = produto.categoria.toLowerCase()
+              return nome.includes(busca) || categoria.includes(busca)
+            }).map(itemProduto)
+        }
+      </div>
+    </div>
+
+    <div className="Carrinho">
+      <h2>Carrinho</h2>
       <div className="Produtos">
-        {produtos.slice(0, 3).map(produto => (
+        {carrinho.map(produto => (
           <div>
             <h3>{produto.nome}</h3>
             <img src={"http://localhost:9000" + produto.img} alt={produto.nome}></img>
             <p>{produto.descricao}</p>
             <h4>{produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h4>
-            <div onClick={() => adicinarCarrinho(produtos)} >adicionar ao carrinho</div>
-          </div>
-        ))}
-      </div>
-    </div>
-
-    <div className="lista">
-      <div className="ordenar" onClick={() => ordenarProdutos(produtos)}>Ordenar Preços</div>
-      <div className="produtos">
-        {produtos.slice(3, produtos.length).map(produto => (
-          <div>
-            <h3>{produto.nome}</h3>
-            <img src={"http://localhost:9000" + produto.img} alt={produto.nome}></img>
-            <h4>{produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h4>
-            <div onClick={() => adicinarCarrinho(produtos)} >adicionar ao carrinho</div>
+            <div onClick={() => removerCarrinho(produto)} >Remover</div>
           </div>
         ))}
       </div>
