41687c7fb3e0c585aeae5bfd9975d07e9042c2f3
onesio <onesiojr1@gmail.com>
2021-04-14 21:18:10 -0300

    estilo basico no carrinho

--- src/Carrinho.js
+++ src/Carrinho.js
@@ -1,24 +1,30 @@
 import React from "react";
 
 const Carrinho = (props) => {
-    console.log(props)
-    // return <p>Aqui</p>
-    const removerCarrinho = (produto) => {
-        props.removerCarrinho(produto);
-      }
-    return <div className="Carrinho">
-      <h2>Carrinho</h2>
+  console.log(props)
+  // return <p>Aqui</p>
+  const removerCarrinho = (produto) => {
+    props.removerCarrinho(produto);
+  }
+  return <div>
+    <h2>Carrinho</h2>
+    <div className="Carrinho">
       {props.carrinho.map((produto, index) => (
-        <div key={index}>
-          <h3>{produto.nome}</h3>
-          <img src={"http://localhost:9000" + produto.img} alt={produto.nome}></img>
-          <p>{produto.descricao}</p>
-          <h4>{produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h4>
-          <div onClick={() => removerCarrinho(produto)} >Remover</div>
+        <div className="Produtos" >
+          <div key={index}>
+            <h3>{produto.nome}</h3>
+            <img src={"http://localhost:9000" + produto.img} alt={produto.nome}></img>
+            <p>{produto.descricao}</p>
+            <h4>{produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h4>
+            <div onClick={() => removerCarrinho(produto)} >Remover</div>
+          </div>
         </div>
       ))}
     </div>
-    
+
+
+    <div className="Finalizar">Finalizar Compra</div>
+  </div>
 }
 
 export default Carrinho;
